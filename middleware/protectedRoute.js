b16c81f6d6aa3eb7668989dd2f2886957ec6863d
anjali <anjali.thorat1510@gmail.com>
2025-01-30 07:22:31 +0530

    feat: Adding middleware to handle protected routes

---
+++ middleware/protectedRoute.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const protectedRoute = (req, res, next) => {
+    const token = req.headers["authentication"]
+    if(!token){
+        return res.status(401).json({ message: "No token provided."})
+    }
+
+    try{
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decodedToken;
+        next()
+    }
+    catch(error){
+        return res.status(402).json({ message: "Invalid Token"})
+    }    
+}
+
+module.exports = protectedRoute;
