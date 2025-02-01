0381f10bfdf74f39532ab499de3210b259d05d13
anjali <anjali.thorat1510@gmail.com>
2025-02-01 19:32:26 +0530

    fixed: Corrected the spelling mistake

--- middleware/protectedRoute.js
+++ middleware/protectedRoute.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const protectedRoute = (req, res, next) => {
-    const token = req.headers["authentication"]
+    const token = req.headers["authorization"]
     if(!token){
         return res.status(401).json({ message: "No token provided."})
     }
