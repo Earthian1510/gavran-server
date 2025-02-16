2e45b33cd1c7043693c14367ffa430a934d9e7f0
Anjali Thorat <anjali.thorat1510@gmail.com>
2025-02-17 02:00:17 +0530

    feat: Adding upload image file

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,13 +2,50 @@ const express = require('express')
 const UserDBG = require('../models/user.model')
 const protectedRoute = require('../middleware/protectedRoute')
 const router = express.Router()
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
 router.use(protectedRoute)
+
+const uploadDirectory = path.join(__dirname, '../uploads');
+if(!fs.existsSync(uploadDirectory)){
+    fs.mkdirSync(uploadDirectory, { recursive: true })
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDirectory);
+    },
+    filename: (req, file, cb) => {
+        const fileName = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, fileName + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({ storage })
+
+router.post('/user/upload-image', protectedRoute, upload.single('image'), async(req, res) => {
+    try{
+        if(!req.file){
+            return res.status(400).json({ message: "No file uploaded."})
+        }
+
+        const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+        res.status(200).json({ imageUrl });
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).json({ message: 'Error uploading image.'})
+    }
+})
+
+router.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 router.get('/user/:userId', async (req, res) => {
     const { userId } = req.params;
     try {
-        const user = await UserDBG.findOne({ _id: userId })
+        const user = await UserDBG.findOne(userId)
         if (!user) {
             return res.status(404).json({ message: 'User not found' })
         }
