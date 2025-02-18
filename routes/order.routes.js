402f416898124f4436c5b7f89c828564eef225fe
Anjali Thorat <anjali.thorat1510@gmail.com>
2025-02-19 00:48:34 +0530

    chore: 'Adding .populate() method'

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -18,7 +18,7 @@ router.get('/orders', async(req, res) => {
 router.get('/orders/:userId', async(req, res) => {
     const { userId } = req.params;
     try{
-        const orders = await OrderDBG.find({ userId }).populate('shippingAddress').populate('orderInfo');
+        const orders = await OrderDBG.find({ userId }).populate('shippingAddress').populate('orderInfo.productId');
 
         if(!orders){
             res.status(404).json({message: "orders not found!"} )
@@ -34,7 +34,7 @@ router.get('/orders/:userId', async(req, res) => {
 router.get('/orders/:userId/:orderId', async (req, res) => {
     const { userId, orderId } = req.params;
     try{
-        const order = await OrderDBG.findOne({ userId, _id: orderId }).populate('shippingAddress').populate('orderInfo');
+        const order = await OrderDBG.findOne({ userId, _id: orderId }).populate('shippingAddress').populate('orderInfo.productId');
 
         if(!order){
             return res.status(404).json({ message: "order not found!"})
